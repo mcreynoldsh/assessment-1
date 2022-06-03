8b4c2cb9283626d1b41e637691c6d3565440f564
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-06-03 15:19:06 -0500

    solution for assessment-1

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,58 @@
-# Write your solution here!
+
+#function which takes in an item's cost and the amount paid as arguments and returns a string giving the optimal amount of change owed
+def optimal_change(item_cost,amount_paid):
+    change_amount = amount_paid - item_cost
+    change_list = []    
+    unique_list = []
+    change_output = f"The optimal change for an item that costs ${'{:.2f}'.format(item_cost)} with an amout paid of ${'{:.2f}'.format(amount_paid)} is " #start of every output string with formatting for correct display of cents decimal places
+    #dictionary with bill-name keyed to monetary amount it represents 
+    money_conv = {           
+        '$100 bill': 100,
+        '$50 bill': 50,
+        '$20 bill': 20,
+        '$10 bill': 10,
+        '$5 bill': 5,
+        '$1 bill': 1,
+        'quarter': .25,
+        'dime': .10,
+        'nickel': .05,
+        'penny': .01
+    }
+
+    #statements to account for edge cases where not enough money given or exact change
+    if change_amount == 0:
+        change_output += "nothing! You gave exact change."
+    elif change_amount < 0:
+        change_output += "nothing! You owe us more money."
+
+    #loop for converting change_amount using dictionary and adding bill-names into new list. Uses round to account for floating point errors
+    for x in money_conv:
+        while change_amount >= money_conv[x]:
+            change_list.append(x)
+            change_amount -= money_conv[x]
+            change_amount = round(change_amount,2)
+
+    #loop for building list of unique bill names        
+    for k in change_list:
+        if unique_list.count(k) == 0:
+            unique_list.append(k)
+  
+    #loop to build output string using each unique bill-name and their count in change_list
+    for change in unique_list:
+
+        if(len(unique_list)>1 and len(unique_list)-1 == unique_list.index(change)):
+            change_output += "and "
+        
+        if change_list.count(change) > 1 and change == 'penny':
+            change_output += f"{change_list.count(change)} pennies"     
+        elif change_list.count(change) > 1:
+            change_output += f"{change_list.count(change)} {change}s"     
+        else:
+            change_output += f"{change_list.count(change)} {change}"
+       
+        if(len(unique_list)-1 == unique_list.index(change)):
+            change_output += "."
+        else:
+            change_output += ", "    
+
+    return change_output
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,9 @@
-# Write your tests here!
+from optimal_change import optimal_change
+#test cases for optimal_change function, including edge/special case values
+print(optimal_change(32.50, 42.60)== "The optimal change for an item that costs $32.50 with an amout paid of $42.60 is 1 $10 bill, and 1 dime.")
+print(optimal_change(50.53, 10)== "The optimal change for an item that costs $50.53 with an amout paid of $10.00 is nothing! You owe us more money.")
+print(optimal_change(26.78, 26.78)== "The optimal change for an item that costs $26.78 with an amout paid of $26.78 is nothing! You gave exact change.")
+print(optimal_change(146.32, 300)== "The optimal change for an item that costs $146.32 with an amout paid of $300.00 is 1 $100 bill, 1 $50 bill, 3 $1 bills, 2 quarters, 1 dime, 1 nickel, and 3 pennies.")
+print(optimal_change(130, 230)== "The optimal change for an item that costs $130.00 with an amout paid of $230.00 is 1 $100 bill.")
+print(optimal_change(136.66, 525)== "The optimal change for an item that costs $136.66 with an amout paid of $525.00 is 3 $100 bills, 1 $50 bill, 1 $20 bill, 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 1 nickel, and 4 pennies.")
+print(optimal_change(5.06,5.07)== "The optimal change for an item that costs $5.06 with an amout paid of $5.07 is 1 penny.")
